ed075fd96b4de518c11c8cb33dc06256d764defd
viktor <vi.dave@yandex.ru>
2023-05-02 10:48:29 +0300

    [other] fix Style

--- internal/repository/cs/common.go
+++ internal/repository/cs/common.go
@@ -10,11 +10,12 @@ import (
 	"time"
 )
 
-func SendFile(ctx context.Context, bucket *storage.BucketHandle, filename string) (err error) {
+func SendFile(ctx context.Context, bucket *storage.BucketHandle, filename string) error {
 	f, err := os.Open(filename)
 	if err != nil {
 		return fmt.Errorf("os.Open: %w", err)
 	}
+
 	defer func(f *os.File) {
 		err := f.Close()
 		if err != nil {
@@ -22,22 +23,20 @@ func SendFile(ctx context.Context, bucket *storage.BucketHandle, filename string
 		}
 	}(f)
 
-	ctx, cancel := context.WithTimeout(ctx, 3*time.Minute)
+	ctxWithTimeout, cancel := context.WithTimeout(ctx, 3*time.Minute)
 	defer cancel()
 
 	items := strings.Split(filename, "/")
 	o := bucket.Object(items[len(items)-1])
 	o = o.If(storage.Conditions{DoesNotExist: true})
-	wc := o.NewWriter(ctx)
+	wc := o.NewWriter(ctxWithTimeout)
+
 	if _, err = io.Copy(wc, f); err != nil {
 		return fmt.Errorf("io.Copy: %w", err)
 	}
+
 	if err := wc.Close(); err != nil {
 		return fmt.Errorf("Writer.Close: %w", err)
-	}
-
-	if err != nil {
-		return err
 	}
 
 	return nil
